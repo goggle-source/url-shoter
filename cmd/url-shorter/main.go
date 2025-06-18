da3020d858f36f6313e4c38dfb929e2f4737b624
goggle-source <asssv0423348@gmail.com>
2025-06-18 13:19:56 +0300

    "5 save"

--- cmd/url-shorter/main.go
+++ cmd/url-shorter/main.go
@@ -1,21 +1,61 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/url-shoter/iternal/config"
+	"github.com/url-shoter/iternal/lib/logger/slo"
+	"github.com/url-shoter/iternal/storage/sqlite"
+)
+
+const (
+	envLocal = "Local"
+	envDev   = "Dev"
+	envProd  = "Prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
-	
-	//TODO: init logger: slog
+	log := setupLogger(cfg.Env)
+	log.Info("Start server")
 
-	//TODO: init storage: sqlite
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", slo.Err(err))
+		os.Exit(1)
+	}
+
+	_ = storage
 
 	//TODO: init router: chi, chi renger
 
 	//TODO: run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(
+				os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(
+				os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(
+				os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	default :
+		log = slog.New(
+			slog.NewJSONHandler(
+				os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
